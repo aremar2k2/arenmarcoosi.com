7df8a7be2843f72acee76c7b5966ab33fbcd0634
aremar2k2 <aremar2k2@gmail.com>
2024-02-22 15:43:00 -0800

    Add files via upload

--- slides.js
+++ slides.js
@@ -1,20 +1,22 @@
 $(function () {
-	var current = $('#slides > :first');
+    var current = $('#slides > li:first').css('left', '0').show();
 
-	function nextSlide() {
-		var next = current.next();
-		if (next.length == 0) {
-			var prev = current.prevAll();
-			next = $(prev[prev.length - 1]);
-		}
+    function nextSlide() {
+        var next = current.next('li');
+        if (next.length === 0) {
+            next = $('#slides > li:first');
+        }
 
-		current.css('left', '0').animate({'left': '-100%'}, 'slow');
-		next.css('display', 'block').css('top', '0').css('left', $('html').css('width')).animate({'left': '0'}, 'slow');
-			current = next;
-	}
+        current.animate({ left: '-100%' }, 'slow', function() {
+            $(this).hide();
+        });
+        next.css('left', '100%').show().animate({ left: '0' }, 'slow');
 
-	$('body').click(nextSlide);
-	$('a').click(function (event) {
-		event.stopPropagation();
-	});
+        current = next;
+    }
+
+    $('body').click(nextSlide);
+    $('a').click(function (e) {
+        e.stopPropagation();
+    });
 });
